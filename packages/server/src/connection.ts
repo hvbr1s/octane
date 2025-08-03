5274387d8b23c3b29a7fddc4136301c621ee4a38
Dan Jaheny <danjaheny@mac.home>
2025-08-03 15:59:58 +0200

    rpv check

--- packages/server/src/connection.ts
+++ packages/server/src/connection.ts
@@ -2,7 +2,7 @@ import { Connection } from '@solana/web3.js';
 import config from '../../../config.json';
 import { ENV_RPC_URL } from './env';
 
-// Use RPC_URL from environment variable, fallback to config.json
 const rpcUrl = ENV_RPC_URL || config.rpcUrl.trim();
+console.log("RPC: ", rpcUrl.substring(0, 15));
 
 export const connection = new Connection(rpcUrl, 'confirmed');
